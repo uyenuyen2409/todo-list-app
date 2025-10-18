09c99571fadf482c1e14fea9deca3d41ea5dad82
uyenuyen2409 <tdkhanhuyen2006@gmail.com>
2025-10-18 16:09:59 +0700

    Initialize To-Do List application with tasks

---
+++ app.py
@@ -0,0 +1,11 @@
+# Danh sách để lưu các công việc
+tasks = []
+def add_task(task_name):
+    """Thêm một công việc mới vào danh sách."""
+    tasks.append(task_name)
+    print(f"Đã thêm công việc:'{task_name}"')
+# --- Điểm bắt đầu của chương trình ---
+if_name_=="_main_":
+    print("Chào mừng deesn với ứng dụng To-Do List!")
+    add_task("Học bài Git và Github")
+    add_task("Làm bài tập thực hành ở nhà")
